1992ae45ad5818ae185c93ddb2d041ce152d467f
Tea <mie.2015@mail.ru>
2025-04-14 13:40:05 +0300

    http connection

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as session from 'express-session';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: 'http://localhost:3001',  
+  });
+  app.use(
+    session({
+      secret: 'secret_key',
+      resave: false,
+      saveUninitialized: false,
+      cookie: { secure: false }, 
+    }),
+  );
+  await app.listen(3000);
 }
+
 bootstrap();
