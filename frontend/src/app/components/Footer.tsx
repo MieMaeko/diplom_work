b74c5eea345de5d0cd7772dbcafbea3812f0a90e
Tea <mie.2015@mail.ru>
2025-04-23 03:17:21 +0300

    better footer

--- frontend/src/app/components/Footer.tsx
+++ frontend/src/app/components/Footer.tsx
@@ -1,36 +1,41 @@
 export default function Footer(){
     return(
         <footer>
-        <div className="footer_back">
-          <h3>Sweetlana</h3>
-          <div className="footer_menu">
-            <div>
-              <p>Конструктор торта</p>
-              <p>Доставка</p>
-              <p>О нас</p>
-            </div>
-            <div>
-              <p>Торты</p>
-              <p>детские</p>
-              <p>бисквитные</p>
-              <p>свадебные</p>
-              <p>антигравитация</p>
-              <p>в шаре</p>
-              <p>муссовые</p>
-              <p>прочее</p>
-            </div>
-            <div>
-              <p>Десерты</p>
-              <p>капкейки</p>
-              <p>пирожные</p>
-              <p>куличи</p>
-              <p>наборы</p>
-              <p>десерты</p>
-            </div>
-            <div>
-
+        <div className="footer-back">
+          <div className="footer-content">
+            <h3>Sweetlana</h3>
+            <div className="footer-menu">
+              <div>
+                <p>Конструктор торта</p>
+                <p>Доставка</p>
+                <p>О нас</p>
+              </div>
+              <div>
+                <p>Торты</p>
+                <p>детские</p>
+                <p>бисквитные</p>
+                <p>свадебные</p>
+                <p>антигравитация</p>
+                <p>в шаре</p>
+                <p>муссовые</p>
+                <p>прочее</p>
+              </div>
+              <div>
+                <p>Десерты</p>
+                <p>капкейки</p>
+                <p>пирожные</p>
+                <p>куличи</p>
+                <p>наборы</p>
+                <p>десерты</p>
+              </div>
+              <div>
+                <p>Контакты</p>
+                <p>+7 (905) 736 66 61</p>
+                <div className="icons">иконки</div>
+              </div>
             </div>
           </div>
+          
         </div>
       </footer>
     )
