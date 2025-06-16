9203c4dc1c54e3a45866066de135e34a07a40427
Tea <mie.2015@mail.ru>
2025-06-16 13:05:58 +0300

    cors

--- backend/src/config/cors.config.ts
+++ backend/src/config/cors.config.ts
@@ -2,6 +2,7 @@ export const corsOptions = {
   origin: [
     'http://localhost:3000',
     'https://frontend-production-a6d0.up.railway.app',
+    'https://diplom-work-sigma.vercel.app/'
   ],
   credentials: true,
 };
