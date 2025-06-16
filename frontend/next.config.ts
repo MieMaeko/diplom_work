93f4648a0964b2d3ac8032db131b0e66fda02fd6
Tea <mie.2015@mail.ru>
2025-06-16 09:45:13 +0300

    cors

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,18 +1,25 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   async rewrites() {
+//     if (process.env.NODE_ENV === 'development') {
+//       return [
+//         {
+//           source: '/api/:path*',
+//           destination: 'http://localhost:3001/:path*', // локальный backend
+//         },
+//       ];
+//     }
+
+//     return [];
+//   },
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    if (process.env.NODE_ENV === 'development') {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'http://localhost:3001/:path*', // локальный backend
-        },
-      ];
-    }
-
-    return [];
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
