30b70386386cd06e11079a14f9d50eb8ad6b3cf4
Tea <mie.2015@mail.ru>
2025-06-16 09:36:44 +0300

    cors

--- backend/src/config/cors.config.ts
+++ backend/src/config/cors.config.ts
@@ -1,4 +1,7 @@
 export const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: [
+    'http://localhost:3000',
+    'https://frontend-production-a6d0.up.railway.app',
+  ],
   credentials: true,
 };
